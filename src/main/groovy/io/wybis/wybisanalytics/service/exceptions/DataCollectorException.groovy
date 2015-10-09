fd5ee0d138ce2c6c7537b009804c04566f2ed0fe
vteial <vteial@gmail.com>
2015-10-09 18:37:37 +0530

    updating latest changes

--- src/main/groovy/io/wybis/wybisanalytics/service/exceptions/DataCollectorException.groovy
+++
@@ -1,8 +0,0 @@
-package io.wybis.wybisanalytics.service.exceptions
-
-import groovy.transform.InheritConstructors;
-
-@InheritConstructors
-public class DataCollectorException extends Exception {
-
-}
